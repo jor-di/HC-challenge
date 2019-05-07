180543e46ad8697c52c94038c8b54437dc9a81d8
jordi Tabit <jordi.tabit@hotmail.fr>
2019-05-07 12:59:19 +0200

    add request model

---
+++ db/migrate/20190507105720_create_requests.rb
@@ -0,0 +1,15 @@
+class CreateRequests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :requests do |t|
+      t.string :name
+      t.string :email
+      t.string :phone_number
+      t.string :biography
+      t.date :email_confirmed_date
+      t.date :request_expiring_date
+      t.date :contract_starting_date
+
+      t.timestamps
+    end
+  end
+end
