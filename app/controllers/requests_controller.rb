1825629d666f565be315663ddea4b8659042412e
jordi Tabit <jordi.tabit@hotmail.fr>
2019-05-07 17:11:34 +0200

    add requests controller

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,20 @@
+class RequestsController < ApplicationController
+  def new
+    @request = Request.new
+  end
+
+  def create
+    request = Request.new(request_params)
+    if request.save
+      redirect_to root
+    else
+      render new
+    end
+  end
+
+  private
+
+  def request_params
+    params.require('request').permit(:name, :phone_number, :email, :biography)
+  end
+end
